29068d0cdef93decab195619747ff6c33d20cb43
muvunyiduke03 <d.ndamagem@alustudent.com>
2025-02-20 13:55:26 -0800

    Freaky

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for i in range(100):
+    for j in range(i + 1, 100):
+        if i < 98 or j < 99:
+            print("{:02d}, {:02d}".format(i, j), end=", ")
+        else:
+            print("{:02d}, {:02d}".format(i, j))
