2fff3fcc00592df91dc8c622c7c54de2fd1ee4ab
muvunyiduke03 <d.ndamagem@alustudent.com>
2025-02-21 00:36:29 -0800

    Is Lower

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 for i in range(100):
     for j in range(i + 1, 100):
-        if i < 98 or j < 99:
-            print("{:02d}, {:02d}".format(i, j), end=", ")
-        else:
-            print("{:02d}, {:02d}".format(i, j))
+        if i != j:
+            print("{:02d}{:02d}".format(i, j), end=", " if i < 98 or j < 99 else "\n")
