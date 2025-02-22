33159611d407d3377001470cc9e755bdf7e140de
muvunyiduke03 <d.ndamagem@alustudent.com>
2025-02-22 12:38:45 -0800

    Combo

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-for i in range(100):
-    for j in range(i + 1, 100):
-        if i != j:
-            print("{:02d}{:02d}".format(i, j), end=", " if i < 98 or j < 99 else "\n")
+for i in range(10):
+    for j in range(i + 1, 10):
+        if not (i == 8 and j == 9):
+            print(f"{i}{j}", end=", ")
+        else:
+            print(f"{i}{j}")
