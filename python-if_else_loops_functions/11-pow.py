324a03e9d8b05b8d5530fc5527fa5faa948525f9
muvunyiduke03 <d.ndamagem@alustudent.com>
2025-02-22 23:36:38 -0800

    FizzBuzz

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
 def pow(a, b):
-    result = 1
-    if b < 0:
+    if b == 0:
+        return 1
+    elif b < 0:
         a = 1 / a
         b = -b
-        for _ in range(b):
-            result *= a
-            return result
+
+    result = 1
+    for _ in range(b):
+        result *= a
+        return result
+
+print(pow(2, 2))
+print(pow(98, 2))
+print(pow(98, 0))
+print(pow(100, -2))
+print(pow(-4, 5))
