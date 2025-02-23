9359ec85902c231b9d2eb2111c90f091fadd7b74
muvunyiduke03 <d.ndamagem@alustudent.com>
2025-02-22 23:48:53 -0800

    Smile in the mirror

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 for i in range(26):
-    print("{}".format(chr(ord('z') - i) if i % 2 == 0 else chr(ord('Z') - i)), end="")
+    print("{}".format(chr(ord('z') - i) if i % 2 == 0 
+        else chr(ord('Z') - i)), end="")
