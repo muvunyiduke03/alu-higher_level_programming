9928a4ad4300ebb5de958a5263350e469b8486d0
muvunyiduke03 <d.ndamagem@alustudent.com>
2025-02-25 08:56:31 +0200

    New

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -4,7 +4,11 @@ def fake_add(a,b):
     return a - b
 if __name__ == "__main__":
     from add_0 import add
+
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, add(a, b)))
-    print("fake add: {} - {} = {}".format(a, b, fake_add(a, b)))
+
+    real_add_result = add(a, b)
+    fake_add_result = fake_add(a, b)
+
+    print("{} + {} = {}\nFake add: {} - {}".format(a, b, real_add_result, a, b, fake_add_result))
