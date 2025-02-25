72e8a472b3f32e5f3f1bd3c99f4c9473c70227c1
muvunyiduke03 <d.ndamagem@alustudent.com>
2025-02-25 09:17:22 +0200

    main

---
+++ python-import_modules/add_0.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def add(a, b):
+    """my addition function 
+    Args:
+        a: First integer
+        b: Second integer
+    Returns:
+    The return value. a - b
+    """
+    return a - b
