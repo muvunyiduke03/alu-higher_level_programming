08a3c54c6b694af4e3182e2111b07b25574e2932
muvunyiduke03 <d.ndamagem@alustudent.com>
2025-02-25 09:36:59 +0200

    New line

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
 
     a = 10
     b = 5
-    print("{} + {} = {}".format(a, b, add(a, b)))
-    print("{} - {} = {}".format(a, b, sub(a, b)))
-    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} + {} = {}\n".format(a, b, add(a, b)))
+    print("{} - {} = {}\n".format(a, b, sub(a, b)))
+    print("{} * {} = {}\n".format(a, b, mul(a, b)))
     print("{} / {} = {}\n".format(a, b, div(a, b)))
