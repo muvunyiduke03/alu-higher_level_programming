a9ca865cb19edfe7ec090bba1a2a10890523226d
muvunyiduke03 <d.ndamagem@alustudent.com>
2025-02-25 08:49:37 +0200

    new

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+def fake_add(a,b):
+    """Function that returns the result of substracting b from a."""
+    return a - b
 if __name__ == "__main__":
     from add_0 import add
     a = 1
     b = 2
     print("{} + {} = {}".format(a, b, add(a, b)))
-    
+    print("fake add: {} - {} = {}".format(a, b, fake_add(a, b)))
