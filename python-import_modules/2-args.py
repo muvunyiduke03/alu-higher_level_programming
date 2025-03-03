831afbb3aad29ab97308721394ddd68c68f032e3
muvunyiduke03 <d.ndamagem@alustudent.com>
2025-03-03 12:22:53 +0200

    Format

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,13 +2,14 @@
 if __name__ == "__main__":
     import sys
 
-    argv = sys.argv
-    argc = len(argv) - 1
+    argv = sys.argv[1:]
+    argc = len(argv)
 
     if argc == 0:
-        print("0 arguments.")
+        print("Number of arguments: 0")
     elif argc == 1:
         print("1 argument:")
+        print("1: {}".format(argv[0]))
     else:
         print("{} arguments:".format(argc))
     for i in range(1, argc + 1):
