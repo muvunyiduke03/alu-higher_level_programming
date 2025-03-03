0070c80f09edb1f67efe58b4befd46aa8457bc1f
muvunyiduke03 <d.ndamagem@alustudent.com>
2025-03-03 12:37:48 +0200

    New format

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-import sys
-argv = sys.argv[1:]
+if __name__ == "__main__":
+    import sys
+    argv = sys.argv[1:]
 
-argc = len(argv)
+    argc = len(argv)
 
-if argc == 0:
-    print("Number of argument(s): 0.")
-elif argc == 1:
-    print("Number of argument(s): 1 argument:")
-    print("1: {}".format(argv[0]))
-else:
-    print("Number of argument(s): {} arguments:".format(argc))
-    for i in range(argc):
-        print("{}: {}".format(i + 1, argv[i]))
+    if argc == 0:
+        print("Number of argument(s): 0.")
+    elif argc == 1:
+        print("Number of argument(s): 1 argument:")
+        print("1: {}".format(argv[0]))
+    else:
+        print("Number of argument(s): {} arguments:".format(argc))
+        for i in range(argc):
+            print("{}: {}".format(i + 1, argv[i]))
