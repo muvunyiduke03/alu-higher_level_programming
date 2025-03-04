19cf1dcc67531d5d171a36eadcc23affa8780505
muvunyiduke03 <d.ndamagem@alustudent.com>
2025-03-04 20:37:04 +0200

    Delete

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -4,7 +4,8 @@ def delete_at(my_list=[], idx=0):
     if idx < 0 or idx >= len(my_list):
         return my_list
     else:
-        return my_list[:idx] + my_list[idx+1:]
+        del my_list[idx]
+        return my_list
 
 if __name__ == "__main__":
     my_list = [1, 2, 3, 4, 5]
