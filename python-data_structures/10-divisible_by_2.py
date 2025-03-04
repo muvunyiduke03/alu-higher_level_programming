aa9dbd9c7cceb76093b0eda157f05479fe543f8c
muvunyiduke03 <d.ndamagem@alustudent.com>
2025-03-04 20:22:33 +0200

    Divisibility

---
+++ python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+def divisible_by_2(my_list):
+    result = []
+    for num in my_list:
+        result.append(num % 2 == 0)
+    return result
+
+if __name__ == "__main__":
+    my_list = [0, 1, 2, 3, 4, 5, 6]
+    list_result = divisible_by_3(my_list)
+
+    i = 0
+    while i < len(list_result):
+        print("{:d} {:s} divisible by 2".format(my_list[i], "is" if list_result[i] else "is not"))
+        i += 1
