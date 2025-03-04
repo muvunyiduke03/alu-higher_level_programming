7b8f73070d486520fe5d8172c71afb45210ce171
muvunyiduke03 <d.ndamagem@alustudent.com>
2025-03-04 16:18:25 +0200

    New names

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
-import hidden_4
+import importlib.util
 
 if __name__ == "__main__":
+    spec = importlib.util.spec_from_file_location("hidden_4", "./hidden_4.pyc")
+    hidden_4 = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(hidden_4)
+
     names = [name for name in dir(hidden_4) if not name.startswith("__")]
     for name in sorted(names):
         print(name)
