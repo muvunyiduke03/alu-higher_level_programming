88c09d054499b1703dcf6cf77c20a226d3fd3ae9
muvunyiduke03 <d.ndamagem@alustudent.com>
2025-03-07 23:49:55 +0200

    Multiply by 2

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -3,6 +3,6 @@
 
 def multiply_by_2(a_dictionary):
     new_dict = {}
-    for key, value in a_dictionary,items():
+    for key, value in a_dictionary.items():
         new_dict[key] = value * 2
     return new_dict
