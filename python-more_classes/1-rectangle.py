69977a3689ca20009e41eeb22978b32cbd5e9d3c
muvunyiduke03 <d.ndamagem@alustudent.com>
2025-03-08 15:26:15 +0200

    1 Rectangle

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -16,12 +16,12 @@ class Rectangle:
         """Initialize the rectangle with optional width and height"""
         self.width = width
         self.height = height
-    
+
     @property
     def width(self):
         """Retrieve the width"""
         return self.__width
-    
+
     @width.setter
     def width(self, value):
         """Set the width with validation"""
@@ -30,12 +30,12 @@ class Rectangle:
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
-    
+
     @property
     def height(self):
         """Retrieve the height"""
         return self.__height
-    
+
     @height.setter
     def height(self, value):
         """Set the height with validation"""
