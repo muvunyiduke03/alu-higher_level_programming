a0bf46e257c5d0047fce31a45e4b366890d4881d
muvunyiduke03 <d.ndamagem@alustudent.com>
2025-03-08 11:05:37 +0200

    Safe print

---
+++ python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except Exception as e:
+        print("Exception: {}".format(e), file=__import__("sys").stderr)
+        return False
+ 
