76a56d04c0bde9e8c8e6887346a15011b30980ca
muvunyiduke03 <d.ndamagem@alustudent.com>
2025-03-21 13:20:56 +0200

    rectangle

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -18,14 +18,16 @@ class Rectangle(BaseGeometry):
         Initializes a Rectangle instance.
 
         Args:
-            width (int): The width of the rectangle, must be a positive integer.
-            height (int): The height of the rectangle, must be a positive integer.
+            width (int): The width of the rectangle,
+            must be a positive integer.
+            height (int): The height of the rectangle,
+            must be a positive integer.
         """
         self.integer_validator("width", width)
         self.__width = width
         self.integer_validator("height", height)
         self.__height = height
-    
+
     def area(self):
         """
         Calculates and returns the area of the rectangle.
@@ -34,12 +36,13 @@ class Rectangle(BaseGeometry):
             int: The area of the rectangle.
         """
         return self.__width * self.__height
-    
+
     def __str__(self):
         """
         Returns the string representation of the rectangle.
 
         Returns:
-            str: The rectangle description in the format [Rectangle] <width>/<height>.
+            str: The rectangle description in the
+              format [Rectangle] <width>/<height>.
         """
         return f"[Rectangle] {self.__width}/{self.__height}"
