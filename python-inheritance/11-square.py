c09eb8b6b5a58e05eb0ac6ed364a9926e4496798
muvunyiduke03 <d.ndamagem@alustudent.com>
2025-03-21 13:50:13 +0200

    class

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -2,6 +2,8 @@
 """
 Module: 11-square
 Defines a class Square that inherits from rectangle.
+Provides methods to calculate the area of a square 
+and validate its size.
 """
 
 Rectangle = __import__('9-rectangle').Rectangle
@@ -10,7 +12,11 @@ Rectangle = __import__('9-rectangle').Rectangle
 class sqaure(Rectangle):
     """
     A class to represent a square.
-    Inherits from Rectangle.
+    Inherits from Rectangle class.
+
+    Attributes:
+        __size (int): The size of the square's side,
+        validated as a positive integer.
     """
 
     def __init__(self, size):
@@ -20,6 +26,10 @@ class sqaure(Rectangle):
         Args:
             size (int): The size of the square's side,
             must be a position integer.
+        
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than or equal to 0.
         """
         self.integer_validator("size", size)
         self.__size = size
