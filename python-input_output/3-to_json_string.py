6922b6e8f25e92e0b3287fa0152b03c2453e49b3
muvunyiduke03 <d.ndamagem@alustudent.com>
2025-03-21 14:42:39 +0200

    json

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -4,6 +4,7 @@ Module: 3-to_json_string
 Defines a function that returns the JSON representation of an object (string).
 """
 
+
 def to_json_string(my_obj):
     """
     Returns the JSON representation of an object (string).
