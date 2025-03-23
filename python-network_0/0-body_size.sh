932f56f2d928dc9c29b6077f0ee789c6715630b7
muvunyiduke03 <d.ndamagem@alustudent.com>
2025-03-23 17:44:19 +0200

    Bsas

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
-# script to get the size of the body of the response for a given URL
-
-if ["$#" -ne 1 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-url="$1"
-
-# Use curl to send a request, then retrieve and display the size of the body
-curl -s "$url" -o /dev/null -w "%{size_download}\n"
+# Sends a request to the provided URL and gets the size of the response
+curl -s "$1" -o /dev/null -w "%{size_download}\n"
