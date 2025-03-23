54b5630b893f217d795763345f3045c0d96ab0d2
muvunyiduke03 <d.ndamagem@alustudent.com>
2025-03-23 09:00:59 -0700

    Body

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,4 +1,4 @@
-#!bin/bash
+#!/bin/bash
 # sends a GET request to the given URL and displays the body only for a 200 status code
 curl -sL -w "%{http_code}" "$1" -o temp_body \ { read code; if [ "$code" = "200" ]; then cat temp_body; fi; rm -f temp_body; }
 
