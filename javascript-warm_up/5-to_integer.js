6fb57f0c8767f8dbace9c70e6c2c85d853f312c2
muvunyiduke03 <d.ndamagem@alustudent.com>
2025-05-26 20:38:45 +0200

    Add 5-to_integer.js to parse and display command line argument as an integer

---
+++ javascript-warm_up/5-to_integer.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const argument = Number.parseInt(process.argv[2]);
+
+if (Number.isNaN(argument)) {
+    console.log('Not a number');
+}
+else {
+    console.log(`My number: ${argument}`);
+}
