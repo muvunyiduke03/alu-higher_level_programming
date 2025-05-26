1cc26b8549a7bfbeab2c3bd58db9c090416f3c28
muvunyiduke03 <d.ndamagem@alustudent.com>
2025-05-26 05:47:12 -0700

    Arguments

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const args = process.argv.length;
+
+if (args === 0) {
+	console.log('No argument');
+}
+else if (args === 1) {
+	console.log('Argument found');
+}
+else {
+	console.log('Arguments found');
+}
+
