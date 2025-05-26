c984da080a540eef1fc79db3e0f0b5c35ef395bc
muvunyiduke03 <d.ndamagem@alustudent.com>
2025-05-26 19:56:52 +0200

    Fix variable name in argument checks in 2-arguments.js

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -2,13 +2,12 @@
 
 const args = process.argv.length - 2;
 
-if (args === 0) {
+if (argv === 0) {
 	console.log('No argument');
 }
-else if (args === 1) {
+else if (argv === 1) {
 	console.log('Argument found');
 }
 else {
 	console.log('Arguments found');
 }
-
