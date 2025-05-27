04372676d06d499f105f27d81c9676f9c3bb0ac2
muvunyiduke03 <d.ndamagem@alustudent.com>
2025-05-27 20:29:49 +0200

    Refactor second biggest number finder for improved argument handling

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,11 +1,17 @@
 #!/usr/bin/node
-const arguments = process.argv.slice(2).map((arguments) => parseInt(arguments, 10));
 
-if (arguments.length < 2) {
+const args = process.argv.slice(2);
+
+if (args.length < 2) {
   console.log(0);
 } else {
-  const max = Math.max(...arguments);
-  const filtered = arguments.filter(n => n !== max);
+  const nums = [];
+  for (let i = 0; i < args.length; i++) {
+    nums.push(parseInt(args[i], 10));
+  }
+
+  const max = Math.max(...nums);
+  const filtered = nums.filter((n) => n !== max);
   const secondMax = Math.max(...filtered);
   console.log(secondMax);
 }
