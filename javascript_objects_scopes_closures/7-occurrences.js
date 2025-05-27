1638ffc76f81ed77106566c5a6bad8ca01d82dee
muvunyiduke03 <d.ndamagem@alustudent.com>
2025-05-28 00:07:53 +0200

    Fix typo in function name from nbOccurrences to nbOccurences

--- javascript_objects_scopes_closures/7-occurrences.js
+++ javascript_objects_scopes_closures/7-occurrences.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 
-exports.nbOccurrences = function (list, searchElement) {
+exports.nbOccurences = function (list, searchElement) {
   return list.filter(element => element === searchElement).length;
 };
