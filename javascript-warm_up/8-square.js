4cd45ff9d1aab99310ce143b3649105a88a1582a
muvunyiduke03 <d.ndamagem@alustudent.com>
2025-05-27 11:30:34 +0200

    Add initial implementation of square printing in 8-square.js

---
+++ javascript-warm_up/8-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const square = parseInt(process.argv[2]);
+
+if (isNaN(square)) {
+  console.log('Missing size');
+} else if (square > 0) {
+  const row = 'X'.repeat(square);
+
+  for (let i = 0; i < square; i++) {
+    console.log(row);
+  }
+}
+ 
