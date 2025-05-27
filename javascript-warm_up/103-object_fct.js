e484ac8093676d9f00e1b8d10f559db0dc4bdb48
muvunyiduke03 <d.ndamagem@alustudent.com>
2025-05-27 22:27:00 +0200

    Refactor myObject definition to improve structure and readability

--- javascript-warm_up/103-object_fct.js
+++ javascript-warm_up/103-object_fct.js
@@ -1,14 +1,15 @@
 #!/usr/bin/node
-
 const myObject = {
   type: 'object',
-  value: 12,
-  incr: function () {
-    this.value++;
-  }
+  value: 12
+};
+console.log(myObject);
+
+// Attach an anonymous function to `incr`
+myObject.incr = function () {
+  this.value++;
 };
 
-console.log({type: 'object', value: 12});
 myObject.incr();
 console.log(myObject);
 myObject.incr();
