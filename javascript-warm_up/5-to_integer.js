acfe16d35bc36f08298d7e28b231f0ce0f223890
muvunyiduke03 <d.ndamagem@alustudent.com>
2025-05-27 20:49:20 +0200

    Fix formatting in 5-to_integer.js for consistent indentation

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -3,7 +3,7 @@
 const argument = Number.parseInt(process.argv[2]);
 
 if (Number.isNaN(argument)) {
-    console.log('Not a number');
-}else {
-    console.log(`My number: ${argument}`);
+  console.log('Not a number');
+} else {
+  console.log(`My number: ${argument}`);
 }
