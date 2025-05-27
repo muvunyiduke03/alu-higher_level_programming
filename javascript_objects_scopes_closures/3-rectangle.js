8109bd826d47084d8a86734275de6f21cd55b6aa
muvunyiduke03 <d.ndamagem@alustudent.com>
2025-05-27 23:30:09 +0200

    Fix formatting in Rectangle constructor by adding space around assignment

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -4,7 +4,7 @@ class Rectangle {
   constructor (w, h) {
     if (typeof w === 'number' && typeof h === 'number' && w > 0 && h > 0) {
       this.width = w;
-      this.height =h;
+      this.height = h;
     }
   }
 
