9ccf082300ae80cdfb0ed216bcc2fe9017bec290
muvunyiduke03 <d.ndamagem@alustudent.com>
2025-05-28 00:06:20 +0200

    Fix typo in filter function within nbOccurrences method

--- javascript_objects_scopes_closures/7-occurrences.js
+++ javascript_objects_scopes_closures/7-occurrences.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 
 exports.nbOccurrences = function (list, searchElement) {
-  return list.filter(element => elemnt === searchElement).length;
+  return list.filter(element => element === searchElement).length;
 };
