5460a92ac48b551837aaa72d7b619c522cf48368
muvunyiduke03 <d.ndamagem@alustudent.com>
2025-06-03 23:49:11 +0200

    Add 100-starwars_characters.js to fetch and display character names from a specified movie

---
+++ javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,49 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const movieId = process.argv[2];
+if (!movieId) {
+  console.error('Usage: ./100-starwars_characters.js <movie_id>');
+  process.exit(1);
+}
+
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  try {
+    const movie = JSON.parse(body);
+    const characters = movie.characters;
+
+    let count = 0;
+    const names = new Array(characters.length);
+
+    characters.forEach((url, idx) => {
+      request(url, (err, res, charBody) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        try {
+          const character = JSON.parse(charBody);
+          names[idx] = character.name;
+          count++;
+
+          if (count === characters.length) {
+            names.forEach(name => console.log(name));
+          }
+        } catch (e) {
+          console.error(e);
+        }
+      });
+    });
+
+  } catch (e) {
+    console.error(e);
+  }
+});
