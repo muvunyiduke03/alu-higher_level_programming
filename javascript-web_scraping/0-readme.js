90e5620f97b8b87c1a8aa1a62f149d602f7493be
muvunyiduke03 <d.ndamagem@alustudent.com>
2025-06-03 22:28:07 +0200

    Fix typo in readFile function call in 0-readme.js

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 
 const filePath = process.argv[2];
 
-fs.readfile(filePath, 'utf8', (err, data) => {
+fs.readFile(filePath, 'utf8', (err, data) => {
   if (err) {
     console.log(err);
   } else {
