57e78fdf8c3c01fcc6df899b5a36958169990ef5
muvunyiduke03 <d.ndamagem@alustudent.com>
2025-06-03 22:49:23 +0200

    Fix typo in require statement in 3-starwars_title.js

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const request = reuire('request');
+const request = require('request');
 
 const movieId = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
